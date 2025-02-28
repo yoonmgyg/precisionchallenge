9d2edb9bf86f5518696ef44d4b6b0af28abb419e
Namgyung Yoo <71161998+yoonmgyg@users.noreply.github.com>
2025-02-27 19:45:21 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from langchain.document_loaders import TextLoader
+from langchain.document_loaders import PyPDFLoader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
@@ -16,19 +16,17 @@ temperature = st.sidebar.slider("LLM Temperature", 0.0, 1.0, 0.0, 0.1)
 chunk_size = st.sidebar.slider("Document Chunk Size", 500, 2000, 1000, 100)
 chunk_overlap = st.sidebar.slider("Document Chunk Overlap", 50, 500, 100, 50)
 
-
 @st.cache(allow_output_mutation=True)
 def load_documents(file_path, chunk_size, chunk_overlap):
-    # Load the document from a text file
-    loader = TextLoader(file_path)
+    # Load the document from a PDF file
+    loader = PyPDFLoader(file_path)
     documents = loader.load()
     # Split the text into manageable chunks
     text_splitter = CharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
     docs = text_splitter.split_documents(documents)
     return docs
 
-docs = load_documents("FDA_guidance.txt", chunk_size, chunk_overlap)
-
+docs = load_documents("FDA_guidance.pdf", chunk_size, chunk_overlap)
 
 # Make sure your OpenAI API key is stored in Streamlit secrets (st.secrets["OPENAI_API_KEY"])
 embeddings = OpenAIEmbeddings(openai_api_key=st.secrets["OPENAI_API_KEY"])
@@ -36,6 +34,7 @@ vectorstore = FAISS.from_documents(docs, embeddings)
 
 
 llm = OpenAI(temperature=temperature, openai_api_key=st.secrets["OPENAI_API_KEY"])
+
 
 qa_chain = RetrievalQAWithSourcesChain.from_chain_type(
     llm=llm,
